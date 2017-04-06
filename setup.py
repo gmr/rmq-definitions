253af7d807212f8e6e41243c38953cb0834c7895
Gavin M. Roy <gavinr@aweber.com>
2017-04-06 17:02:29 -0400

    setup.py and cfg

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+
+import os.path
+import setuptools
+
+
+def read_requirements(filename):
+    requirements = []
+    try:
+        with open(os.path.join('requires', filename)) as req_file:
+            for line in req_file:
+                if '#' in line:
+                    line = line[:line.index('#')]
+                line = line.strip()
+                if line.startswith('-r'):
+                    requirements.extend(read_requirements(line[2:].strip()))
+                elif line:
+                    requirements.append(line)
+    except IOError:
+        pass
+    return requirements
+
+
+setuptools.setup(
+    name='rmq-definitions',
+    version='1.0.0',
+    description=('Deterministicly sorting and formatting of RabbitMQ '
+                 'definition backups'),
+    author='Gavin M. Roy',
+    author_email='gavinmroy@gmail.com',
+    url='https://github.com/sprockets/sprockets-influxdb',
+    install_requires=read_requirements('installation.txt'),
+    license='BSD',
+    py_modules=['rmq_definitions'],
+    entry_points={'console_scripts': [
+        'rmq-definitions=rmq_definitions:main'
+    ]},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: System :: Archiving :: Backup',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities'],
+    test_suite='nose.collector',
+    tests_require=read_requirements('testing.txt'),
+    zip_safe=True
+)
